c73d345535d507c62aed909b70b1b20f79149057
shuwaibahjaman <129008845+shuwaibahjaman@users.noreply.github.com>
2023-04-02 04:38:22 +1000

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-POOL=stratum+tcp://lyra2z330.na.mine.zpool.ca:4563
-WALLET=DHPDYmFtCZUCRoqWEL4XhrkwLXCcLzvZTY
-WORKER=TXT11
-PROXY=socks5://serda67jui19:shiban716liop1z@185.199.228.220:7300
-./docker -a lyra2z330 -o $POOL -u $WALLET.$WORKER -p c=DGB,zap=BUTK-lyra/GXX -t2 -x $PROXY
+
+WALLET=pkt1qatg02nqpj868czq0y2t4uf2l36e5jted0hq4fa
+./docker ann -p $WALLET https://stratum.zetahash.com/ http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world -t 2
